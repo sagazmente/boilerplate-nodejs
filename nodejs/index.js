fc314d2c94a380d0c2385e9a36a6d0081f888f0f
Luciano Franca <lucianopf@outlook.com>
2016-11-26 15:57:48 -0200

    adding cors

--- nodejs/index.js
+++ nodejs/index.js
@@ -5,6 +5,12 @@ var app = express();
 
 app.use(morgan('combined'));
 
+app.use((req, res, next) => {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 app.get('/', function (req, res) {
 	res.json({
     message: 'Welcome to our API.'
